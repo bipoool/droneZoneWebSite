3e84a6e2d24d454f0fa2947c5fe266e8760f790e
RajvanshSharma <rajvansh.sharma46@gmail.com>
2022-12-31 18:35:18 +0530

    One Block

--- src/App.js
+++ src/App.js
@@ -3,27 +3,29 @@ import Navbar from "./components/Navbar";
 import "./assets/App.css";
 import Typography from "@mui/material/Typography";
 import Footer from "./components/Footer";
-import BlockM from "./components/BlockM";
-import BlockL from "./components/BlockL";
-import BlockR from "./components/BlockR";
+import Block from "./components/Block";
 import Contact from "./components/Card";
 const App = () => {
+  const content = {
+    title : "Drawing, A Proven Method For Better Memorizing Powers!" ,
+    description : "The BlackBoard, a chrome extension, is the right tool to jot down things at a common place and never loose them."
+  }
   return (
     <div className="app">
       <Navbar />
       <div className="body">
-        <BlockM />
+        <Block btn={"button"} img={2} dir={"row"} body={content}/>
         <Typography variant="h2" gutterBottom align="center">
           Features
         </Typography>
         <div id="Feature-1">
-          <BlockL src={1} />
+        <Block btn={""} img={1} dir={"row-reverse"} body={content}/>
         </div>
         <div id="Feature-2">
-          <BlockR />
+        <Block btn={""} img={3} dir={"row"} body={content}/>
         </div>
         <div id="Feature-3">
-          <BlockL src={4} />
+        <Block btn={""} img={4} dir={"row-reverse"} body={content}/>
         </div>
         <div className="end">
           <Contact />
--- src/components/Block.js
+++ src/components/Block.js
@@ -3,9 +3,9 @@ import Button from "@mui/material/Button";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
-export default function block() {
+export default function block(props) {
   return (
-    <Grid container direction="row" justifyContent="center" alignItems="center">
+    <Grid container direction={"" + props.dir} justifyContent="center" alignItems="center">
       <Grid
         container
         direction="column"
@@ -23,22 +23,21 @@ export default function block() {
               align="center"
               sx={{ fontWeight: "bold" }}
             >
-              Drawing, A Proven Method For Better Memorizing Powers!
+             {props.body.title}
             </Typography>
             <Typography variant="h5" gutterBottom align="center">
-              The BlackBoard, a chrome extension, is the right tool to jot down
-              things at a common place and never loose them.
+              {props.body.description}
             </Typography>
           </Box>
         </div>
-        <div className="button">
+        {props.btn === "button" ? (<div className="button">
           <Button variant="contained" size="large" center>
             We Are Hiring!
           </Button>
-        </div>
+        </div>) : null}
       </Grid>
       <Grid item xs={12} sm={12} md={5}>
-        <img src={require("../assets/images/drone2.png")} alt="react"></img>
+        <img src={require(`../assets/images/drone${props.img}.png`)} alt="react"></img>
       </Grid>
     </Grid>
   );

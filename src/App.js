5d6b0c862976f335d0c0cbfdda67338e5f804396
--global <--global>
2023-01-01 18:00:12 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -6,27 +6,38 @@ import Footer from "./components/Footer";
 import Block from "./components/Block";
 import Contact from "./components/Card";
 const App = () => {
-  const content = {
+  const content = [{
     title : "Drawing, A Proven Method For Better Memorizing Powers!" ,
-    description : "The BlackBoard, a chrome extension, is the right tool to jot down things at a common place and never loose them."
-  }
+    description : "The BlackBoard, a chrome extension, is the right tool to jot down things at a common place and never loose them.",
+    type: "row"
+  },
+  {
+    title : "Drawing, A Proven Method For Better Memorizing Powers!" ,
+    description : "The BlackBoard, a chrome extension, is the right tool to jot down things at a common place and never loose them.",
+    type: "row-reverse"
+  },
+  {
+    title : "Drawing, A Proven Method For Better Memorizing Powers!" ,
+    description : "The BlackBoard, a chrome extension, is the right tool to jot down things at a common place and never loose them.",
+    type: "row"
+  }];
   return (
     <div className="app">
       <Navbar />
       <div className="body">
-        <Block btn={"button"} img={2} dir={"row"} body={content}/>
+        <Block btn={"button"} img={2} dir={"row"} body={content[0]}/>
         <Typography variant="h2" gutterBottom align="center">
           Features
         </Typography>
-        <div id="Feature-1">
-        <Block btn={""} img={1} dir={"row-reverse"} body={content}/>
-        </div>
-        <div id="Feature-2">
-        <Block btn={""} img={3} dir={"row"} body={content}/>
-        </div>
-        <div id="Feature-3">
-        <Block btn={""} img={4} dir={"row-reverse"} body={content}/>
-        </div>
+
+        {
+          content.map((e, id) => (
+            <div id={`Feature-${id+1}`}>
+              <Block btn={""} img={id+1} dir={e.type} body={e}/>
+            </div>
+          ))
+        }
+  
         <div className="end">
           <Contact />
         </div>

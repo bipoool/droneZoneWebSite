4bee22eb434e5cf265bd93fbac8a4b73ae805f70
RajvanshSharma <rajvansh.sharma46@gmail.com>
2023-01-01 21:22:22 +0530

    images

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ const App = () => {
     <div className="app">
       <Navbar />
       <div className="body">
-        <Block btn={"button"} img={2} dir={"row"} body={content[0]}/>
+        <Block btn={"button"} img={4} dir={"row"} body={content[0]}/>
         <Typography variant="h2" gutterBottom align="center">
           Features
         </Typography>
